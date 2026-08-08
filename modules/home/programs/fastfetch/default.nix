333e95c7b17e0b3ad9db0b3376a1ea3f1c80b9ee
Kanta IMAI <work@floating-gate.com>
2026-08-09 05:50:36 +0900

    chore: add comment of fastfetch alias

--- modules/home/programs/fastfetch/default.nix
+++ modules/home/programs/fastfetch/default.nix
@@ -3,6 +3,8 @@
 {
   programs.fastfetch.enable = true;
 
+  # The width-aware fastfetch alias that selects between these configs is
+  # defined in ../fish.nix.
   # Replace the pre-existing, unmanaged config on the first activation.
   xdg.configFile = {
     "fastfetch/config.jsonc" = {
