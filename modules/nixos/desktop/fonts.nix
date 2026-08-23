1e3a04d4360fb7b0423214ef8a23658542ff8ede
canta-9142 <work@floating-gate.com>
2026-08-23 20:18:10 +0900

    chore: change kmscon font size

--- modules/nixos/desktop/fonts.nix
+++ modules/nixos/desktop/fonts.nix
@@ -1,36 +1,37 @@
 { config, pkgs, ... }:
 
 {
-    fonts = {
-    	fontDir.enable = true;
+  fonts = {
+    fontDir.enable = true;
 
-		packages = with pkgs; [
-			noto-fonts
-			noto-fonts-cjk-sans
-			noto-fonts-cjk-serif
-			noto-fonts-color-emoji
-			cascadia-code
-		];
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      cascadia-code
+    ];
 
-		fontconfig = {
-			enable = true;
-			defaultFonts = {
-				sansSerif = [ "Noto Sans CJK JP" ];
-				serif = [ "Noto Sans CJK JP" ];
-				monospace = [
-					"Cascadia Code NF"
-					"Noto Sans Mono CJK JP"
-				];
-				emoji = [ "Noto Color Emoji" ];
-			};
-		};
-	};
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "Noto Sans CJK JP" ];
+        serif = [ "Noto Sans CJK JP" ];
+        monospace = [
+          "Cascadia Code NF"
+          "Noto Sans Mono CJK JP"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 
-	services.kmscon = {
-		enable = true;
-		config = {
-			font-engine = "pango";
-			font-name = "Cascadia Code NF";
-		};
-	};
+  services.kmscon = {
+    enable = true;
+    config = {
+      font-engine = "pango";
+      font-name = "Cascadia Code NF";
+      font-size = 17;
+    };
+  };
 }
