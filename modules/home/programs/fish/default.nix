be43adc4911a4d061ba29cb6278d734d9b7af945
canta-9142 <work@floating-gate.com>
2026-09-21 18:03:25 +0900

    feat: add git abbrs

--- modules/home/programs/fish/default.nix
+++ modules/home/programs/fish/default.nix
@@ -38,10 +38,15 @@
       gcam = "git commit -a -m";
       gco = "git checkout";
       gd = "git diff";
+      gds = "git diff --staged";
       gf = "git fetch";
       gl = "git log --graph --all --pretty=format:'%Cred%h%Creset %Cgreen(%cI) -%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset' --abbrev-commit --date=rfc2822";
       gpl = "git pull";
       gp = "git push";
+      gr = "git restore --staged .";
+      grs = "git reset --soft HEAD^";
+      grm = "git reset --mixed HEAD^";
+      grh = "git reset --hard HEAD^";
       gs = "git status";
       gst = "git stash";
       gsw = "git switch -c";
