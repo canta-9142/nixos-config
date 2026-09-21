ee215f5443e1fc4b68c47d1af90066bf8438de9a
canta-9142 <work@floating-gate.com>
2026-09-21 21:34:14 +0900

    feat: confine Zathura with AppArmor

--- modules/nixos/security/apparmor.nix
+++ modules/nixos/security/apparmor.nix
@@ -1,3 +1,16 @@
+{ pkgs, ... }:
+
 {
-  security.apparmor.enable = true;
+  security.apparmor = {
+    enable = true;
+    policies.zathura = {
+      state = "enforce";
+      # Compile at build time as well as at activation; never load policy here.
+      path = pkgs.runCommand "zathura-apparmor" { nativeBuildInputs = [ pkgs.apparmor-parser ]; } ''
+        apparmor_parser --skip-kernel-load --skip-cache \
+          -I ${pkgs.apparmor-profiles}/etc/apparmor.d ${./zathura.apparmor}
+        cp ${./zathura.apparmor} "$out"
+      '';
+    };
+  };
 }
