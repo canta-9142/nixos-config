8a92d6f0566b8927a8b58c62d311f7bd00d70200
canta-9142 <work@floating-gate.com>
2026-09-21 22:31:29 +0900

    feat: add fish functions

--- modules/home/programs/fish/default.nix
+++ modules/home/programs/fish/default.nix
@@ -4,29 +4,37 @@
   programs.fish = {
     enable = true;
 
-    functions.fastfetch = ''
-      set -l options
-      if test "$COLUMNS" -le 130
-        set -a options --config "${config.xdg.configHome}/fastfetch/narrow.jsonc"
-      end
+    functions = {
+      ".." = ''
+        cd "../$argv[1]"
+      '';
+      "..." = ''
+        cd "../../$argv[1]"
+      '';
+      "...." = ''
+        cd "../../../$argv[1]"
+      '';
+      fastfetch = ''
+        set -l options
+        if test "$COLUMNS" -le 130
+          set -a options --config "${config.xdg.configHome}/fastfetch/narrow.jsonc"
+        end
 
-      if contains -- "$TERM" xterm-kitty xterm-ghostty; or contains -- "$TERM_PROGRAM" kitty ghostty
-        set -a options --logo-type kitty-direct
-      else
-        set -a options --logo-type builtin --logo nixos
-      end
+        if contains -- "$TERM" xterm-kitty xterm-ghostty; or contains -- "$TERM_PROGRAM" kitty ghostty
+          set -a options --logo-type kitty-direct
+        else
+          set -a options --logo-type builtin --logo nixos
+        end
 
-      ${pkgs.fastfetch}/bin/fastfetch $options $argv
-    '';
+        ${pkgs.fastfetch}/bin/fastfetch $options $argv
+      '';
+    };
 
     shellAbbrs = {
       nb = "nh os boot";
       ns = "nh os switch";
       nsu = "nh os switch --update";
       mi = "micro";
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
       g = "git";
       ga = "git add";
       gaa = "git add .";
